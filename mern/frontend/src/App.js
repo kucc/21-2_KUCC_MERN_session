1e53e5f7c45797ca09a17e34bf5df5275c933488
keira-park <ynnus411@naver.com>
2021-09-25 15:16:57 +0900

    Create App.js

---
+++ mern/frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Body from './Body';
+import Footer from './Footer';
+
+function App() {
+  const state = {
+    name: '박선영',
+    age: 22,
+    hobby: 'drum',
+    major: 'DIS',
+    contact: 'ynnus411@naver.com',
+  };
+
+  return (
+    <div className="App">
+      <Header name={state.name}/>
+      <Body age={state.age} hobby={state.hobby} major={state.major}/>
+      <Footer contact={state.contact} />
+    </div>
+  );
+}
+
+export default App;
