4101ba67a5e3883d9099612b85f4d2dd67862fcd
keira-park <ynnus411@naver.com>
2021-09-25 15:57:49 +0900

    Update Body.js

--- mern/frontend/src/Body.js
+++ mern/frontend/src/Body.js
@@ -1,9 +1,11 @@
-function Body(props) {
+import React from 'react';
+
+function Body({age, hobby, major}) {
     return (
         <>
-            <div>저는 {props.age >= 30 ? '30대' : '20대'}입니다.</div>
-            <div>{props.hobby}를 좋아합니다.</div>
-            <div>전공은 {props.major}입니다.</div>
+           <div>저는 {age > 30 ? '30대' : '20대'}입니다.</div>
+            <div>{hobby}를 좋아합니다.</div>
+            <div>전공은 {major}입니다.</div>
         </>
     );
 }
