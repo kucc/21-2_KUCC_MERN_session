d1b2dd477d780faa9ca8e12859baea3d121db099
keira-park <ynnus411@naver.com>
2021-09-25 15:58:25 +0900

    Update Header.js

--- mern/frontend/src/Header.js
+++ mern/frontend/src/Header.js
@@ -1,14 +1,16 @@
-function Header(props) {
+import React from 'react';
+
+function Header() {
     const state = {
-        title: "21-2 MERN Session",
+        title: '21-2 MERN Session',
     };
     return (
         <>
         <div>{state.title}</div>
-        <h2>저는 {props.name}입니다.</h2>
+        <h2>저는 {state.name}입니다.</h2>
         </>
-    )
+    );
 }
-
 // return 값은 하나의 태그로 묶어줘야 한다.
+
 export default Header;
