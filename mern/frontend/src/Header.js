b8eb973cde8c3dfa056aeed67cfa372f317edd7e
keira-park <ynnus411@naver.com>
2021-09-25 23:02:09 +0900

    Update Header.js

--- mern/frontend/src/Header.js
+++ mern/frontend/src/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-function Header() {
+function Header(props) {
     const state = {
         title: '21-2 MERN Session',
     };
     return (
         <>
         <div>{state.title}</div>
-        <h2>저는 {state.name}입니다.</h2>
+        <h2>저는 {props.name}입니다.</h2>
         </>
     );
 }
