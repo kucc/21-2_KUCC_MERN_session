076e849d3001b8ffb4a2dcddf73a74dd42dd8dfd
keira-park <ynnus411@naver.com>
2021-10-15 01:56:15 +0900

    Add link to home, link to about

--- mern/frontend/src/App.js
+++ mern/frontend/src/App.js
@@ -6,6 +6,14 @@ import Body from './Body';
 import Footer from './Footer';
 import {useState} from 'react';
 import Input from './Input';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from './Home';
+import About from './About';
 
 function App() {
   const state = {
@@ -18,7 +26,7 @@ function App() {
   const [greeting, setGreeting] = useState("안녕하세요");
   // setGreeting에 실행되는 순간 리렌더링, 함수 다시 호출, setGreeting한 값을 greeting에 대입한 후 return문 렌더링
   const handleChange = event => setGreeting(event.target.value)
-  return (
+  /* return (
     <div className="App">
       <Header name={state.name} />
       <Body age={state.age} hobby={state.hobby} major={state.major} />
@@ -29,7 +37,31 @@ function App() {
         />
       <h1>{greeting}</h1>
     </div>
+  ); */
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </div>
   );
+
+
 }
 
 export default App;
