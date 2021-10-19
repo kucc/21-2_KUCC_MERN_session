c595a80bb6903323e99393fb895510575b8072ce
keira-park <ynnus411@naver.com>
2021-10-19 23:43:00 +0900

    Move to practice file

--- mern/frontend/src/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer = (props) => (
-    <div>{props.contact}</div>
-);
-
-export default Footer;
--- mern/frontend/src/Input.js
+++
@@ -1,6 +0,0 @@
-const Input = (props) => {
-    const {value,onChange} = props;
-    return <input type="text" value={value} onChange={onChange} />;
-};
-
-export default Input;
