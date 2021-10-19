805bc59ab98513dc0854b4ddeb3e2e1038d81021
keira-park <ynnus411@naver.com>
2021-10-19 23:44:17 +0900

    Create Board.js

---
+++ mern/frontend/src/Board.js
@@ -0,0 +1,10 @@
+const Board = () => {
+    return(
+        <div className="Title">
+            <h1>Board</h1>
+            <h3>여기는 게시판 페이지입니다.</h3>
+        </div>
+    );
+};
+
+export default Board;
