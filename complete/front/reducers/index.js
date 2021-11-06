b6b7f50a21f12f8c72dfe900e7e25433ca376235
Taewoong1378 <ktw2378@korea.ac.kr>
2021-11-06 17:58:04 +0900

    prettier + eslint 적용

--- complete/front/reducers/index.js
+++ complete/front/reducers/index.js
@@ -4,17 +4,17 @@ import user from './user';
 
 // (이전상태, 액션) => 다음 상태를 만들어내는 함수
 const rootReducer = (state, action) => {
-    switch (action.type) {
-        case HYDRATE:
-            // console.log('HYDRATE', action);
-            return action.payload;
-        default: {
-            const combinedReducer = combineReducers({
-                user,
-            });
-            return combinedReducer(state, action);
-        }
+  switch (action.type) {
+    case HYDRATE:
+      // console.log('HYDRATE', action);
+      return action.payload;
+    default: {
+      const combinedReducer = combineReducers({
+        user,
+      });
+      return combinedReducer(state, action);
     }
+  }
 };
 
 export default rootReducer;
